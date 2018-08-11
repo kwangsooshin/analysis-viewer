0168d5021e62da9fd3fce3d68347a2c721398aca
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-08-12 01:45:43 +0900

    Modify Module_manager model: Add part of saving contents of csv file

--- Module_Manager/models.py
+++ Module_Manager/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+import csv
 
 
 class Modules (models.Model):
 	module_name = models.TextField(blank=True)
 	module_korname = models.TextField(blank=True)  # 한글
-	csv_file = models.URLField()
+	# csv_file = models.FileField(upload_to='/modules/csv')
+	csv_file = models.TextField(blank=True)
+	url = models.URLField(default='')
+
+	def save(self, *args, **kwargs):
+		# read csv file
+		fp = open(self.csv_file, 'r', encoding='utf-8')
+		fpr = csv.reader(fp)
+		for line in fpr:
+			N_eng = line[1]
+			N_kor = line[2]
+
+			# EngtoKor_list.objects.create(Modules_model=self, Eng_name=N_eng, Kor_name=N_kor)
+			self.name_list.create(Eng_name=N_eng, Kor_name=N_kor)
+		fp.close()
+
 
 
 class EngtoKor_list (models.Model):
-	Modules_model = models.OneToOneField(Modules, related_name='name_list', on_delete=models.CASCADE)
+	Modules_model = models.ForeignKey(Modules, related_name='name_list', on_delete=models.CASCADE)
 	Eng_name = models.TextField(blank=True)
 	Kor_name = models.TextField(blank=True)
