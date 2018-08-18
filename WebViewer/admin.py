36a8419c6449a5f7ca468e6b8a8c5c470cd6dde4
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-08-19 00:57:34 +0900

    change app name (WebViewer -> AnalysisViewer)

--- WebViewer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ImageModel, ResultModel, ResultPositionModel, ResultLabelModel
-
-
-admin.site.register(ImageModel)
-admin.site.register(ResultModel)
-admin.site.register(ResultPositionModel)
-admin.site.register(ResultLabelModel)
--- WebViewer/static/js/demo/result_list.js
+++
@@ -1,3 +0,0 @@
-var resultArea = create_list()
-create_row(resultArea, "Position", "x: 0, y: 0, w: 90, h: 180")
-create_row(resultArea, "label", "people" +  ", " + "table")
