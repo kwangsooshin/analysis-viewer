61eb03df2813ed377f4770e647815f6e4abe5532
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-08-19 04:04:40 +0900

    Add and modify code to read module name (from model)

--- utils/get_names.py
+++ utils/get_names.py
@@ -3,16 +3,15 @@ from Module_Manager.models import Modules
 
 
 def read_modules():
-	url = 'http://mlcoconut.sogang.ac.kr:8000/modules/group/'
+	url = 'http://mlcoconut.sogang.ac.kr:8000/module/group/'
 	response = requests.get(url)
-	print("response: "+str(response))
-	# jsonDatas = response.json()
-	# print(jsonDatas)
 
-	names = ['initial']
-	# for jsonData in jsonDatas:
-	# 	names.append(jsonData['name'])
+	jsonDatas = response.json()['results']
+	names = []
+	for jsonData in jsonDatas:
+		names.append(jsonData['name'])
 	return names
+
 
 
 def read_modules_model():
@@ -20,5 +19,5 @@ def read_modules_model():
 	modules = dict()
 	for i in items:
 		modules[i.module_name] = i.module_korname
-
+	print(modules)
 	return modules
